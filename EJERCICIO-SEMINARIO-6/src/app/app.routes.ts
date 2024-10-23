418019178f6a2ab19d85c80c3dab38ccb1efb0b3
Bryan834 <bryan.garcia@estudiantat.upc.edu>
2024-10-23 23:44:55 +0200

    asignaturaSevice.ts vacía. profesorService.ts creada con CRUD y otras funcionalidades

--- EJERCICIO-SEMINARIO-6/src/app/app.routes.ts
+++ EJERCICIO-SEMINARIO-6/src/app/app.routes.ts
@@ -8,9 +8,9 @@ import { BuscadorComponent } from './componentes/buscador/buscador.component';
 export const routes: Routes = [
     //ruta por defecto
     {path: '',redirectTo:'home', pathMatch: 'full'},//ruta por defecto
-    {path: 'home', component: HomeComponent},
-    {path: 'profesor', component: ProfesorComponent},
-    { path: 'buscador', component: BuscadorComponent },
+    {path: 'home', component: HomeComponent},//ruta a home
+    {path: 'profesor', component: ProfesorComponent}, //ruta a profesor
+    { path: 'buscador', component: BuscadorComponent },//ruta a buscador
     {path : '**', redirectTo: 'home'}//ruta por defecto
 
 
@@ -20,4 +20,5 @@ export const routes: Routes = [
     imports: [RouterModule.forRoot(routes, {useHash: true})],//useHash: true para que funcione en github
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {}/*exporta la clase AppRoutingModule para que se pueda utilizar 
+                                en otros ficheros de la aplicación*/
