1d841613ca447b755d3d6810a354bf11ba80e09a
Bryan834 <bryan.garcia@estudiantat.upc.edu>
2024-10-23 23:29:22 +0200

    app.routes.ts configurado para todos los componentes

--- EJERCICIO-SEMINARIO-6/src/app/app.routes.ts
+++ EJERCICIO-SEMINARIO-6/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProfesorComponent} from './componentes/profesor/profesor.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { BuscadorComponent } from './componentes/buscador/buscador.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //ruta por defecto
+    {path: '',redirectTo:'home', pathMatch: 'full'},//ruta por defecto
+    {path: 'home', component: HomeComponent},
+    {path: 'profesor', component: ProfesorComponent},
+    { path: 'buscador', component: BuscadorComponent },
+    {path : '**', redirectTo: 'home'}//ruta por defecto
+
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, {useHash: true})],//useHash: true para que funcione en github
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
